40b7340c17f88b317d0817e25818371fbb12138b
Mul-L <93707369+Mul-L@users.noreply.github.com>
2021-11-04 18:42:41 +0900

    Add files via upload

---
+++ .github/workflows/Ch16/getDataClass.py
@@ -0,0 +1,37 @@
+import csv 
+import datetime
+
+class GetData:
+    def __init__(self, file):
+        self.file = file
+    
+    def readFile(self):
+        list = []
+        with open(self.file) as f:
+            reader = csv.reader(f)
+            header = next(f)           
+            for row in reader:
+                list.append(row)
+        return list
+
+    def getRains(self, reader): 
+        rains = []      
+        for row in reader:
+            try:
+                rain = float(row[3])
+            except ValueError:
+                print(f"Error happened at {row[2]}")
+            else:
+                rains.append(rain)
+        return rains
+
+    def getDates(self, reader):  
+        dates = []     
+        for row in reader:
+            try:
+                date = datetime.datetime.strptime(row[2], '%Y-%m-%d') 
+            except ValueError:
+                print(f"Error happened at {date}")
+            else:
+                dates.append(date)
+        return dates
